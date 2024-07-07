7725c91346b0f31bc02a5b78c953bf5f30d62a4a
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2024-07-07 14:23:36 +0800

    remove build folders + files

--- build/lib/endpoints/__init__.py
+++
@@ -1,5 +0,0 @@
-from .authorization import Authorization
-from .servers import Servers
-from .virtual_machines import VirtualMachines
-from .containers import Containers
-from .billing import Billing
